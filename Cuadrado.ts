f9c3e54497e94b6d96e21295cfe8debde33b5960
Miguel300l <miguelledezma400@gmail.com>
2023-08-02 21:27:14 -0500

    actualizado cuadrado e index

--- Cuadrado.ts
+++ Cuadrado.ts
@@ -17,5 +17,4 @@ export class Cuadrado{
         return 4 * this.lado;
       }
 
-
 }
---
+++ index.ts
@@ -0,0 +1,5 @@
+
+import { Cuadrado } from "./Cuadrado";
+const miCuadrado = new Cuadrado(9);
+  console.log("area del cuadrado:", miCuadrado.calcularArea());
+  console.log("perimetro del cuadrado:", miCuadrado.calcularPerimetro());
